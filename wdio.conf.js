a8d45c10b9bed2377bd8b3cf70aad55439b6f050
Thavananthan Nanthu <nnanthu49@gmail.com>
2024-05-27 12:15:59 +0530

    chanages added ios

--- wdio.conf.js
+++ wdio.conf.js
@@ -24,7 +24,7 @@ exports.config = {
     //
     specs: [
         // ToDo: define location for spec files here
-        './test/specs/**/ios*.js'
+        './test/specs/**/ios-native*.js'
     ],
     // Patterns to exclude.
     exclude: [
